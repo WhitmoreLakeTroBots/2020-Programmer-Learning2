c9b2c59bbcad89c9a54644edb81a94cdce6ca717
WhitmoreLakeTroBots <wlhsroboticsteam@gmail.com>
2020-01-11 15:52:33 -0500

    SK I made a program that crys

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # 2020-Programmer-Learning2
 
 
-this if for java learning in robotics, where I am currently learning about git
+"this if for java learning in robotics, where I am currently learning about git
+I am also experiencing life as a scared peice of minced mutton 
+help me help me helpme helpm e hepl me hme eeee e ee e ee ee" 
+	~soren
---
+++ Robomath.java
@@ -0,0 +1,27 @@
+
+/******************************************************************************
+ *  Compilation:  javac cry, no math.java
+ *  Execution:    java cry, no math
+ *
+ *  Prints "cry, no math". By tradition, this is only soren's first program.
+ *
+ *  % java cry, no math
+ *  cry, no math
+ *
+ *  These 17 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class Robomath {
+
+    public static void main(String[] args) {
+        // Prints "cry, no math" to the terminal window.
+        System.out.println("cry, no math");
+    }
+
+}
